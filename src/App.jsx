9707ea17d33192fe772c51929d6c7c60075ce131
Dennis-83 <dodo_ez@hotmail.com>
2024-11-19 21:19:17 +0100

    Added Tile component

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,15 @@
 import './App.css'
 import Button from "./assets/components/Button.jsx";
 import Product from "./assets/components/Product.jsx";
+import Tile from "./assets/components/Tile.jsx";
 
 function App() {
+    // Tekst voor de paragrafen altijd in een array ivm .map methode in Tile component,
+    // ook bij een enkele paragraaf. (evt op te lossen door strings naar array te converteren, dan kan enkele paragraaf ook in een string)
+    const singleParagraph =["Lorem ipsum dolor sit amet, consectetur adipisicing elit. Ad alias aut cumque ea hic impedit, laboriosam nostrum odio perferendis porro temporibus ut voluptatum. Accusamus deserunt dolor dolore dolorum enim eum ex exercitationem expedita illum ipsum nostrum quae quis rem, totam unde ut, veritatis. Doloremque eius magni similique suscipit veritatis voluptatem."]
+    const multipleParagraphs = ["Lorem ipsum dolor sit amet, consectetur adipisicing elit. Eaque et inventore nesciunt saepe suscipit. Accusantium eveniet incidunt nisi omnis possimus quisquam sequi ullam unde voluptatum!", "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Ad asperiores consectetur cum eius in ipsum molestias, nihil pariatur possimus quis recusandae vero voluptate, voluptates voluptatibus?"]
+
+
     return (
         <>
             <h1>Handbags & Purses</h1>
@@ -55,6 +62,24 @@ function App() {
                     price="€150,-"
                 />
             </main>
+            <footer>
+                <Tile
+                    title="The Brand"
+                    tileText={multipleParagraphs}
+                />
+                <Tile
+                    image="../src/assets/brand.png"
+                    alt="brand"
+                />
+                <Tile
+                    image="../src/assets/our_story.png"
+                    alt="our story"
+                />
+                <Tile
+                    title="Our Story"
+                    tileText={singleParagraph}
+                />
+            </footer>
         </>
     )
 }
---
+++ src/assets/components/Tile.jsx
@@ -0,0 +1,41 @@
+import React from "react";
+
+function Tile({title, tileText, image, alt}) {
+
+    // Korte versie
+    return (
+        <section>
+            {image ? (
+                <img src={image} alt={alt}/>
+            ) : (
+                <>
+                    <h2>{title}</h2>
+                    {/*Meerdere paragrafen kunnen zo worden toegevoegd(of een enkele, mits in een array*/}
+                    {tileText.map((paragraph) => (
+                        <p>{paragraph}</p>
+                    ))}
+                </>
+            )}
+        </section>
+    )
+
+    // Lange versie
+    // if (image) {
+    //     return (
+    //         <section>
+    //             <img src={image} alt={alt}/>
+    //         </section>
+    //     )
+    // } else {
+    //     return (
+    //         <section>
+    //             <h2>{title}</h2>
+    //             {tileText.map((paragraph) => (
+    //                 <p>{paragraph}</p>
+    //             ))}
+    //         </section>
+    //     )
+    // }
+}
+
+export default Tile;
